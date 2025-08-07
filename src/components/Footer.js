3ac84e171f5adc91ce6676ca76143de6c4957362
Gabriele Ricci <gabrielericci234@gmail.com>
2025-08-07 17:42:47 +0200

    modifica footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,8 +18,7 @@ export default function Footer() {
           
           {/* Informazioni fiscali */}
           <div className="footer-fiscal-info">
-            <p>Codice Fiscale: 03230700597</p>
-            <p>Partita IVA: 03230700597</p>
+            <p>PI / CF: 03230700597</p>
             <p>Emailpec: <a href="mailto:mcbalance@pec.it">mcbalance@pec.it</a></p>
           </div>
           
